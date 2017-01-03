99de2f29014402550a69aa60c81e0aa3a3debf80
michaelplott <mike.plott80@gmail.com>
2017-01-03 01:23:00 -0500

    Initial push

---
+++ src/main/java/com/CCL/CclApplication.java
@@ -0,0 +1,12 @@
+package com.CCL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CclApplication.class, args);
+	}
+}
