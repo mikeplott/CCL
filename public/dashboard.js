9945fb5b8e7e154a5f51cc91d7fb43c0f08aa37a
mikeplott <mike.plott80@gmail.com>
2017-01-10 14:44:52 -0500

    One more

--- public/dashboard.js
+++ public/dashboard.js
@@ -3205,11 +3205,7 @@ if (theBeer != null) {
 
       theContainer.append(myCont);
 
-      console.log('this is a test!');
-      console.log('this is another test!');
-      console.log('Yet another test!');
-      console.log('last test!');
-      console.log('jk one more');
+      
 
     //  $('#myContainer').append(theBtn);
   }
