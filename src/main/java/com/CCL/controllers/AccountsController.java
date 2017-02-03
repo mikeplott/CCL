933dd9cee4905c584e4877676591378371c72226
michaelplott <mike.plott80@gmail.com>
2017-02-03 17:37:58 -0500

    Deleted beer wrapper class. production about to continue.

--- src/main/java/com/CCL/controllers/AccountsController.java
+++ src/main/java/com/CCL/controllers/AccountsController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
